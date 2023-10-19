69454f4bd7dd7827337daa1d9e6e6a9d834a835d
Joshua <joshua.miasco@gmail.com>
2023-10-19 18:47:01 +0800

    update

--- src/main/java/com/roseta/core/repository/AccountRepository.java
+++ src/main/java/com/roseta/core/repository/AccountRepository.java
@@ -3,6 +3,4 @@ package com.roseta.core.repository;
 import com.roseta.core.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Accounts, Integer> {
-
-}
+public interface AccountRepository extends JpaRepository<Accounts, Integer> {}
